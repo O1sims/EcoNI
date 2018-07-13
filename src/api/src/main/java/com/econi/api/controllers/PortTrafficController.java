e13c402bdd15da4396c9aa1e619164e2dba5b821
Owen Sims <owen.sims@cyberlytic.com>
2018-07-13 10:59:37 +0100

    Remove unused models and repos

---
+++ src/api/src/main/java/com/econi/api/controllers/PortTrafficController.java
@@ -0,0 +1,37 @@
+package com.econi.api.controllers;
+
+import com.econi.api.models.PortTraffic;
+import com.econi.api.repositories.PortTrafficRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/port-traffic")
+public class PortTrafficController {
+  @Autowired
+  private PortTrafficRepository repository;
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public List<PortTraffic> getAllPortTraffic() {
+    return repository.findAll();
+  }
+
+  @RequestMapping(value = "/{year}", method = RequestMethod.GET)
+  public List<PortTraffic> getPetById(@PathVariable("year") Integer year) {
+    return repository.findByYear(year);
+  }
+  
+  @RequestMapping(value = "/{year}", method = RequestMethod.PUT)
+  public void modifyPortTrafficByYear(@PathVariable("year") Integer year, @Valid
+  @RequestBody PortTraffic portTraffic) {
+    repository.save(portTraffic);
+  }
+}
